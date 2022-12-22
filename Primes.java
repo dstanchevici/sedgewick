c168ec161ee781dcb7243b1c3d746e5af192e39f
Dmitri Stanchevici <dmitristanchevici@Dmitris-iMac.local>
2022-12-22 14:50:53 -0500

    Added Primes

---
+++ Primes.java
@@ -0,0 +1,20 @@
+package ch3;
+
+public class Primes {
+    public static void main(String[] args) {
+        int N = 32;
+        boolean[] a = new boolean [N];
+        for (int i = 2; i < N; i++) a[i] = true;
+
+        for (int i = 2; i < N; i++)
+            if (a[i] != false)
+                for (int j = i; j * i < N; j++)
+                    a[i*j] = false;
+
+        for (int i = 2; i < N; i++)
+            if (i > N - 100)
+                if (a[i]) System.out.println(" " + i);
+
+        System.out.println();
+    }
+}
