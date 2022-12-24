addb07b7cf092ef6c71b18a9855524d088d5aec4
Dmitri Stanchevici <dstnchvc@castechs-MBP.fios-router.home>
2022-12-24 18:56:04 -0500

    Added a try-catch statement to ensure sufficient memory

--- Primes.java
+++ Primes.java
@@ -1,18 +1,15 @@
-// More changes Dec 23
-
-<<<<<<< HEAD
-//package ch3;
-// Changes on home laptop
-=======
-// Add a comment to see how changes are reflected in GitHub repo
-
-package ch3;
->>>>>>> c67af9479f03393b567806299a870e3884c127c7
 
 public class Primes {
     public static void main(String[] args) {
         int N = 32;
-        boolean[] a = new boolean [N];
+        boolean[] a;
+        try {
+            a = new boolean [N];
+        }
+        catch (OutOfMemoryError e) {
+            System.out.println ("Out of memory");
+            return;
+        }
         for (int i = 2; i < N; i++) a[i] = true;
 
         for (int i = 2; i < N; i++)
